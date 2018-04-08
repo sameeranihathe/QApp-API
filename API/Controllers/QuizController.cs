1e78141105b0d14799cd5b3bfe165dec2dbe95e5
sameeranihathe <sameeranihathe@gmail.com>
2018-04-08 06:40:11 +0530

    controllers created

---
+++ API/Controllers/QuizController.cs
@@ -0,0 +1,55 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class QuizController : ApiController
+    {
+        [HttpGet]
+        [Route("api/Questions")]
+        public HttpResponseMessage GetQuestions()
+        {
+            using (DBModel db = new DBModel())
+            {
+                var Qns = db.Questions
+                    .Select(x => new { QnID = x.Qn,  x.Qn,  x.ImageName, x.Option1, x.Option2, x.Option3, x.Option4 })
+                    .OrderBy(y => Guid.NewGuid())
+                    .Take(10)
+                    .ToArray();
+
+                var updated = Qns.AsEnumerable()
+                    .Select(x => new
+                    {
+                        x.QnID,
+                        x.Qn,
+                        x.ImageName,
+                        Options = new string[] { x.Option1, x.Option2, x.Option3, x.Option4 }
+
+                    }).ToList();
+                return this.Request.CreateResponse(HttpStatusCode.OK, updated);
+
+            }
+        }
+
+        [HttpPost]
+        [Route("api/Answers")]
+        public HttpResponseMessage GetAnswers(int[] qIDs)
+        {
+            using (DBModel db = new DBModel())
+            {
+                var result = db.Questions
+                    .AsEnumerable()
+                    .Where(y => qIDs.Contains(y.QnID))
+                    .OrderBy(x => { return Array.IndexOf(qIDs, x.QnID})
+                    .Select(z => z.Answer)
+                    .ToArray();
+
+                return this.Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+    }
+}
